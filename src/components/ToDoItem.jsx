fe4c6f6b6cc4895192274833d7ed39e9accf6bd4
Monika Śleboda <monikakozak60@gmail.com>
2023-08-31 11:22:31 +0200

    Add strikethrough and remove it after clicking on an item

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 
+
 function ToDoItem (props){
-    return <li>{props.text}</li>
+
+    const [isDone, setIsDone] = useState(false);
+
+    function handleClick(){
+        setIsDone (prevvalue => {
+            return !prevvalue;
+        });
+    }
+
+
+    return(
+        <div onClick={handleClick}>
+            <li style={{ textDecoration: isDone ? "line-through" : "none" }}>{props.text}</li>
+        </div>
+    );
 }
+
+
+
 
 
 export default ToDoItem;
